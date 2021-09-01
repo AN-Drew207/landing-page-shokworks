d35ba57c0d98c0d99262e20d0d81102acd5a9476
AN-Drew207 <andrescontrerasoviedo740@gmail.com>
2021-09-01 16:56:53 -0400

    navbar mobile and  tablet view finished

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,15 +1,58 @@
 import React, {useState, useEffect} from 'react' 
 import logo from '../img/Group 2486@2x.png'
+import logomini from '../img/logo-min.png'
 
 const NavbarComponent = () =>{
+
+    const [collapse, setCollapse]=useState(false);
+    const [burguerButtonActive, setBurguerButtonActive] = useState("none");
+    const [logoImg, setLogoImg] = useState(logo)
+
+    useEffect(() =>{
+        if(window.screen.width<1400){
+            setCollapse(()=>true);
+        }
+        if(window.screen.width<=700){
+            setLogoImg(logomini)
+        }
+        window.addEventListener('resize', ()=>{
+            if(window.screen.width<1400){
+                setCollapse(()=>true);
+            }else{
+                setCollapse(()=>false);
+            }
+            if(window.screen.width<700){
+                setLogoImg(logomini)
+            }else{
+                setLogoImg(logo)
+            }
+        })
+    }, [])
+
+    /*Función handle para manejar el estado del burguer menu button*/ 
+    const handleBurguer = () =>{
+        document.querySelector("#burguer-btn").classList.toggle("burguer-btn-active")
+        document.querySelector("#burguer-btn").classList.toggle("burguer-btn")
+        setBurguerButtonActive((prev)=>{
+            if(prev==="none"){
+                return "inline-block"
+            }else{
+                return "none"
+            }
+        });
+        document.querySelector("#burguer-collapse").classList.toggle("navbar-nav-container-collapse")
+        document.querySelector("#burguer-collapse").classList.toggle("navbar-nav-container-collapsed")
+    }
 
     return( 
         <>
         <nav id="nav">
             <div className="navbar-img-container">
-                <img className="logo" src={logo} alt="logo" />
+                <img className="logo" src={logoImg} alt="logo" />
             </div>
             <div className="navbar-content">
+                {
+                collapse ||
                 <div className="navbar-nav-container">
                     <ul className="navbar-nav">
                         <li className="navbar-nav-item active">
@@ -32,14 +75,43 @@ const NavbarComponent = () =>{
                         </li>
                     </ul>
                 </div>
+                }
                 <div className="navbar-search">
                     <input className="search" type="text" placeholder="Search here"/>
                 </div>
-                <div>
+                <div className="btn-group">
                     <button className="contact-us-btn">Contact Us</button>
                 </div>
+                {
+                    collapse &&
+                <div className="burguer-btn" id="burguer-btn" onClick={handleBurguer}>
+                    <span></span>
+                </div>
+                }
             </div>
         </nav>
+            <div id="burguer-collapse" style={{display:burguerButtonActive}}>
+                    <ul className="navbar-nav-collapse">
+                        <li className="navbar-nav-item-collapse active-collapse">
+                            Home
+                        </li>
+                        <li className="navbar-nav-item-collapse">
+                            About
+                        </li>
+                        <li className="navbar-nav-item-collapse">
+                            Portfolio
+                        </li>
+                        <li className="navbar-nav-item-collapse">
+                            Product
+                        </li>
+                        <li className="navbar-nav-item-collapse">
+                            Career                       
+                        </li>
+                        <li className="navbar-nav-item-collapse">
+                            Blog
+                        </li>
+                    </ul>
+                </div>
         </>
     )
 }
